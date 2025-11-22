63db588f989cf0c5da5a150a305215f99bc895d1
Harrison <harrison831123@gmail.com>
2025-11-22 15:28:45 +0800

    調整註解

--- Web API/SignalRAPI/SignalRAPI/SignalRHub.cs
+++ Web API/SignalRAPI/SignalRAPI/SignalRHub.cs
@@ -9,42 +9,17 @@ namespace SignalRAPI
 {
     public class SignalRHub : Hub
     {
-        // ... 其他 Hub 方法
-
-        // /// <summary>
-        // /// 发送给指定组
-        // /// </summary>
-        // public void CallGroup(string fromname, string content)
-        // {
-        //     string groupname = Context.QueryString["groupname"]; // 获取客户端发送过来的用户名
-        //     // 根据username获取对应的ConnectionId
-        //     Clients.Group(groupname).show(fromname + ":" + content);
-        // }
-
         /// <summary>
-        /// 当客户端成功连接到 Hub 时触发
+        /// 當客戶端成功連線到 Hub 時觸發
         /// </summary>
         public override Task OnConnected()
         {
-            // 獲取已驗證的用戶名
-            //string username = Context.User.Identity.Name;
-
-            //if (string.IsNullOrEmpty(username))
-            //{
-
-            //    username = "TESTER_A001";
-            //}
-
-            //if (!string.IsNullOrEmpty(username))
-            //{
-            //    // 現在，無論是否登入，它都會使用 "TESTER_A001" 來加入群組
-            //    Groups.Add(Context.ConnectionId, username);
-            //    // 您可以在這裡加入 Console.WriteLine($"Connection ID: {Context.ConnectionId} joined Group: {username}");
-            //}
+            // 從 Query String 獲取用戶 ID
             var username = Context.QueryString["userid"];
 
             if (!string.IsNullOrEmpty(username))
             {
+                // 將此 Connection ID 加入到以 username 為名的群組中
                 Groups.Add(Context.ConnectionId, username);
             }
 
@@ -52,20 +27,18 @@ namespace SignalRAPI
         }
 
         /// <summary>
-        /// 当客户端断开连接时触发
+        /// 當客戶端斷開連線時觸發
         /// </summary>
         public override Task OnDisconnected(bool stopCalled)
         {
-            // 1. 获取已验证的用户名
-            string username = Context.User.Identity.Name;
+            // 1. 獲取已驗證的用戶名稱 
+            //string username = Context.User.Identity.Name;
+            var username = Context.QueryString["userid"];
 
             if (!string.IsNullOrEmpty(username))
             {
-                // 2. 将此连接从用户群组中移除
+                // 2. 將此連線從用戶群組中移除
                 Groups.Remove(Context.ConnectionId, username);
-
-                // 建议：在此处移除 Cache 中的 ConnectionId 记录
-                // 例如：YourCacheService.Remove(username);
             }
 
             return base.OnDisconnected(stopCalled);
